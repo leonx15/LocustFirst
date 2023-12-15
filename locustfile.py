a5fd722e79ad52ec08a3faf204f5240d50abf116
TLeoniuk <Tomasz.Leoniuk@gds.ey.com>
2023-12-15 13:53:42 +0100

    Adjustments to hold external certificates.

--- locustfile.py
+++ locustfile.py
@@ -17,6 +17,11 @@ class UserBehavior(HttpUser):
         super().__init__(*args, **kwargs)
         self.should_logout = False
 
+    def on_start(self):
+        # In this place we set a path to our certificate to allow https requests.
+        # if not needed please comment line below.
+        self.client.verify = 'cert/certific.pem'
+
     @task(1)
     def login(self):
         login_url = "/authenticate"
